5ca2f44a310fb3d034ddd74e25db7b6131e6976e
Varun Yadav <onlinevarunyadav@gmail.com>
2026-08-19 14:45:02 +0530

    Add programs to find largest and smallest elements, calculate sum and average, and count odd and even numbers in an array

---
+++ oddEven.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class oddEven {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even elements: " + even);
+        System.out.println("Odd elements: " + odd);
+        sc.close();
+    }
+}
